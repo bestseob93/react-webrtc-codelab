e7a809714a779bc344f18ceea2ff12cb78db5830
Hwan Seob Lee <bestseob93@gmail.com>
2017-08-21 00:18:46 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+### webrtc codelab
+https://bitbucket.org/webrtc/codelab/src/fc1f8b8b5e87?at=master
+
+### to react webrtc codelab
---
+++ webrtc-codelab/step1/server.js
@@ -0,0 +1,6 @@
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+var app = http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(3000, (cb) => console.log('server is runnig on 3000'));
